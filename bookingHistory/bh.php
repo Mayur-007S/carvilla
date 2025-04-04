bbf2be93323eeada27c2dbb8b9a77b3eb39cea2f
mayur_007 <mayurmyana111@gmail.com>
2025-04-04 23:34:42 +0530

    modifie home page.solve booking history problem.delete unused files.

--- bookingHistory/bh.php
+++ bookingHistory/bh.php
@@ -1,5 +1,4 @@
 <?php 
-require_once 'Order-list.php';
 
 $host = "localhost";
 $port = 3306;
@@ -13,17 +12,26 @@ $con = new mysqli($host, $user, $password, $dbname, $port, $socket);
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 }
+$user_email = "";
+$SQL = "SELECT * FROM user";
+$result = $con->query($SQL);
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $user_email = $row['email'];
+    }
+} else {
+    echo "0 results";
+}
 
-$user_email = $con->real_escape_string($user_email);
 $SQL = "SELECT * FROM bookings WHERE email='$user_email'";
 $result = $con->query($SQL);
 
 if ($result->num_rows > 0) {
-    echo "<h1>Booking Data</h1>";
+    echo "<h1>Booking History</h1>";
     echo "<table border='1px solid black'><tr><th>Id</th><th>carName</th><th>model</th><th>price</th><th>clientId</th><th>name</th><th>email</th><th>phone</th><th>age</th><th>address</th><th>showroom</th></tr>";
     while($row = $result->fetch_assoc()) {
-        echo "<tr><td>".$row['id']."</td><td>".$row['carName']."</td><td>".$row['model']."</td><td>".$row['price']."</td>
-        <td>".$row['clientId']."</td><td>".$row['name']."</td><td>".$row['email']."</td><td>".$row['phone']."</td>
+        echo "<tr><td>".$row['bookingID']."</td><td>".$row['carName']."</td><td>".$row['model']."</td><td>".$row['price']."</td>
+        <td>".$row['clientId']."</td><td>".$row['username']."</td><td>".$row['email']."</td><td>".$row['phone']."</td>
         <td>".$row['age']."</td><td>".$row['address']."</td><td>".$row['showroom']."</td>
         </tr>";
     }
--- contact/Contact.php
+++ contact/Contact.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CarVilla - Home</title>
     <link rel="icon" type="png" href="assets/logo/favicon.png">
-    <link rel="stylesheet" href="prefixCSS.css">
+    <link rel="stylesheet" href="../prefixCSS.css">
     <!-- <link rel="stylesheet" href="style1.css"> -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -176,11 +176,11 @@
     <section>
         <div class="form-container">
             <div class="main-form">
-                <div class="logo-brand" style="display: flex; gap: 10px;">
-                    <img src="./assets/logo/favicon.png" alt="logo" style="width: 25px; height: 25px;">
+                <div class="logo-brand" style="display: flex; gap: 10px; ">
+                    <img src="../assets/logo/favicon.png" alt="logo" style="width: 25px; height: 25px;">
                     <h1 style="color: black;">CARVILLA</h1>
                 </div>
-                <div class="form-title" style="margin-top: 2rem; width:20rem;">
+                <div class="form-title" style=" width:20rem; margin-top: 1rem;">
                     <h1>We'd love to help</h1>
                     <p style="font-size: 14px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ex totam, aliquid modi inventore
                         obcaecati ad sequi vero, voluptas earum voluptatibus optio. Officiis, laudantium delectus.</p>
@@ -197,35 +197,33 @@
                         </div>
                         <div class="filed"
                             style="display: flex; gap: 10px;justify-content:space-between;    width: 68.5%;">
-                            <label for="phone">Phone no</label> <label for="address">Address</label>
+                            <label for="phone">Phone no</label> 
                         </div>
                         <div class="phone-address filed" style="margin-bottom: 1.5rem;">
-                            <input type="text" name="phone" id="phone" placeholder="Phone" required>
-                            <input type="text" name="address" id="address" placeholder="Address" required>
+                            <input type="text" name="phone" id="phone" placeholder="Phone" required style="width: 100%;">
                         </div>
-                        <div class="filed" style="display: flex; gap: 10px;justify-content:space-between;width: 72.5%;">
-                            <label for="cname">Car name</label> <label for="cmodel">Car model</label>
+                        <div class="filed"
+                            style="display: flex; gap: 10px;justify-content:space-between; width: 68.5%;">
+                            <label for="address">Address</label>
                         </div>
-                        <div class="cname-cmodel filed" style="margin-bottom: 1.5rem;">
-                            <input type="text" name="carName" id="cname" placeholder="Car name" required>
-                            <input type="text" name="model" id="cmodel" placeholder="Car model" required>
+                        <div class="phone-address filed" style="margin-bottom: 1.5rem;">
+                        <input type="text" name="address" id="address" placeholder="Address" style="width: 100%;" required>
                         </div>
-                        <div class="ccolor">
-                            <select name="color">
-                                <option value="red">Red</option>
-                                <option value="blue">Blue</option>
-                                <option value="green">Green</option>
-                                <option value="black">Black</option>
-                                <option value="white">White</option>
-                                <option value="yellow">Yellow</option>
-                            </select>
+
+                        <div class="filed"
+                            style="display: flex; gap: 10px;justify-content:space-between; width: 68.5%;">
+                            <label for="feedback">Feedback</label>
                         </div>
+                        <div class="phone-address filed" style="margin-bottom: 1rem;">
+                        <input type="text" name="feedback" id="feedback" placeholder="feedback" style="width: 100%;" required>
+                        </div>
+                       
                         <button type="submit">Submit</button>
                     </form>
                 </div>
             </div>
             <div class="form-carousel">
-                <img src="./assets/images/background.png" alt="" width="100%" height="100%" style="border-radius: 6%;">
+                <img src="../assets/images/background.png" alt="" width="100%" height="100%" style="border-radius: 6%;">
                 <div class="form-carousel-inner" style="display: flex;position: absolute;top: 24rem;">
                     <div class="rate-desc-auth">
                         <p class="form-rate">⭐⭐⭐⭐⭐</p>
@@ -250,12 +248,12 @@
                         rotate: 180px;
                         transform: rotate(268deg);"
                     >
-                    <img src="./assets/icons/icons8-up-96.png" width="40" height="40"></button>
+                    <img src="../assets/icons/icons8-up-96.png" width="40" height="40"></button>
                         <button style="background: none;
                         border: none;
                         rotate: 180px;
                         transform: rotate(90deg);">
-                        <img src="./assets/icons/icons8-up-96.png" width="40" height="40"></button>
+                        <img src="../assets/icons/icons8-up-96.png" width="40" height="40"></button>
                     </div>
                 </div>
                
@@ -266,7 +264,7 @@
 
     <section>
         <div class="background-img">
-            <img src=".\assets\images\form\formlogo.svg" alt="" width="80%" height="80%"
+            <img src="../assets/images/form/formlogo.svg" alt="" width="80%" height="80%"
                 style=" position: absolute;
                         top: 7rem;
                        left: 26.7rem;
